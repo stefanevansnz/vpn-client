7639255b954ed51239b65482c97a0aa9aceaa178
SE <stefan.evans@consegna.cloud>
2022-10-18 10:25:47 +1300

    added public subnet

--- vpn_client/vpn_client_stack.py
+++ vpn_client/vpn_client_stack.py
@@ -18,19 +18,28 @@ class VpnClientStack(Stack):
         self.vpc = ec2.Vpc(self, "ClientVPNVPC",
                            max_azs=1,
                            cidr="192.168.0.0/16",
-                           subnet_configuration=
-                            [  ec2.SubnetConfiguration(
+                           subnet_configuration=[ 
+                           ec2.SubnetConfiguration(
                                subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
                                name="PrivateSubnetVPNTarget",
                                cidr_mask=24
-                            )                           
+                           )                           
                            ]
                         )
 
         # Create VPN Endpoint and Associate with VPC
-        endpoint = self.vpc.add_client_vpn_endpoint("VPNClientEndpoint",
+        self.endpoint = self.vpc.add_client_vpn_endpoint("VPNClientEndpoint",
             cidr="10.10.0.0/16",
             server_certificate_arn="arn:aws:acm:ap-southeast-2:915922766016:certificate/1d88cc91-aa0c-4a2e-b2fc-86a4e96ab358",
             client_certificate_arn="arn:aws:acm:ap-southeast-2:915922766016:certificate/b4abb268-abeb-4153-81dd-467378f6e404",
             logging=False  
         )
+
+        # Create new public subnet and add to VPC
+        self.private_subnet = self.vpc.select_subnets(subnet_group_name="PrivateSubnetVPNTarget").subnets[0]
+        self.public_subnet = ec2.PublicSubnet(self, "PublicSubnetVPNApp",
+            vpc_id=self.vpc.vpc_id,
+            availability_zone=self.private_subnet.availability_zone,
+            cidr_block='192.168.100.0/24')
+        
+
